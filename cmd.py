9d4887af6664a702b2d9288e94489b0aa3e1a91f
Mihai Ionut Vilcu <ionutvmi@gmail.com>
2014-08-30 00:05:09 +0300

    Added the powershell option for cmd

--- cmd.py
+++ cmd.py
@@ -3,11 +3,15 @@ Open cmd in the directory of the current file
 """
 import os, sublime_plugin
 class CmdCommand(sublime_plugin.TextCommand):
-    def run(self, edit):
+    def run(self, edit, powershell = False):
         file_name=self.view.file_name()
         path=file_name.split("\\")
         current_driver=path[0]
         path.pop()
         current_directory="\\".join(path)
-        command= "cd "+current_directory+" & "+current_driver+" & start cmd"
+        cmd = 'cmd'
+        if powershell:
+            cmd = 'powershell'
+        command= "cd "+current_directory+" & "+current_driver+" & start %s" % cmd
         os.system(command)
+
